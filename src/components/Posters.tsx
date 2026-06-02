68fbf41edea443398576a2441f0469ef7239acca
Ramakrishna Reddy Palle <21ee01026@iitbbs.ac.in>
2026-06-03 02:33:11 +0530

    added social preview metadata

--- src/components/Posters.tsx
+++ src/components/Posters.tsx
@@ -1,4 +1,18 @@
-import '../assets/styles/Posters.scss';
+import React from "react";
+
+import poster01 from "../assets/images/poster01.png";
+import poster02 from "../assets/images/poster02.png";
+import poster03 from "../assets/images/poster03.png";
+import poster04 from "../assets/images/poster04.png";
+
+import "../assets/styles/Posters.scss";
+
+const posters = [
+  poster01,
+  poster02,
+  poster03,
+  poster04,
+];
 
 function Posters() {
 
@@ -11,7 +25,48 @@ function Posters() {
 
       <div className="posters-container">
 
-        <h1>Posters</h1>
+        <div className="posters-header">
+
+          <p className="posters-tag">
+            THE CAMPAIGN
+          </p>
+
+          <h1>
+            Ideas. Vision. <br />
+            Action.
+          </h1>
+
+          <p className="posters-description">
+
+            Leadership is not about promises alone,
+            it is about direction, responsibility,
+            and action. These campaign messages reflect
+            the vision and commitment behind every step
+            towards OSMECON 2026.
+
+          </p>
+
+        </div>
+
+        <div className="posters-grid">
+
+          {posters.map((poster, index) => (
+
+            <div
+              className="poster-card"
+              key={index}
+            >
+
+              <img
+                src={poster}
+                alt={`Campaign Poster ${index + 1}`}
+              />
+
+            </div>
+
+          ))}
+
+        </div>
 
       </div>
 
